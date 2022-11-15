fa431f3c4dfba83e444098cd9912705fb0d164a7
Laurareis1 <116307113+Laurareis1@users.noreply.github.com>
2022-11-15 14:39:51 +0000

    exer

--- src/componentes/PhoneBook.js
+++ src/componentes/PhoneBook.js
@@ -6,7 +6,9 @@ function PhoneBook() {
 
     const nameRef = useRef();
     const phoneRef = useRef();
+    const searchRef = useRef();
     const [phoneBookList, setPhoneBookList] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
     console.log(phoneBookList);
 
 
@@ -24,10 +26,33 @@ function PhoneBook() {
         console.log(phoneBookList);
 
         setPhoneBookList([...phoneBookList]);
+        nameRef.current.value=null;
+        phoneRef.current.value=null;
     }
 
-    return <>
-    <div className="conteiner">
+    function search() {
+        let results = [];
+        const searchValue = searchRef.current.value;
+        
+        for (let contact of phoneBookList) {
+            if(contact.name.toLowerCase().includes(searchValue.toLowerCase())) {
+                results.push(contact);
+            }
+        }
+        setSearchResult(results);
+    }
+
+    function clearSearch() {
+        searchRef.current.value=null;
+        setSearchResult([]);
+    }
+
+    function removeContact(i) {
+        phoneBookList.splice(i, 1);
+        setPhoneBookList([...phoneBookList]);
+    }
+
+    return <div className="wrapper">
         <div className="left">
             <div>
                 <label>
@@ -37,24 +62,38 @@ function PhoneBook() {
             </div>
             <div>
                 <label>
-                    Phone number:
-                    <input ref={phoneRef} type="telephone" />
+                    Phone:
+                    <input ref={phoneRef} type="tel" />
                 </label>
                 <button onClick={add}>Add</button>
             </div>
+
+            <h4>My contact list:</h4>
+            {phoneBookList.map((contact, i)=>
+                <div key={i} className="contact-item">
+                    Name: {contact.name}<br/>
+                    Phone: {contact.phone}
+                 <button onClick={()=>removeContact(i)}>Delete</button>
+                </div>
+            )}
         </div>
 
         <div className="right">
-            {phoneBookList.map((contact, i)=>
+            <input ref={searchRef} type="text" />
+            <button onClick={search}>Search</button>
+            <button onClick={clearSearch}>Clear</button>
+
+            <h4>Search result:</h4>
+
+            {searchResult.map((contact, i)=>
                 <div key={i} className="contact-item">
                     Name: {contact.name}<br/>
                     Phone: {contact.phone}
                 </div>
             )}
+            
         </div>
-
     </div>
-    </>
 }
 
 export default PhoneBook;
